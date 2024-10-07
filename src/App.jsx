44220448ee0fdc335f77fa4d47480f4da01c74c5
Antonina Sych <antoninasych@gmail.com>
2024-10-07 11:06:31 +0100

    Browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // Вже не потрібен BrowserRouter тут
 import AppRoutes from './routes';
 import './css/App.css';
 
 function App() {
     return (
-        <Router>
+        <>
             <nav>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/movies">Movies</NavLink>
             </nav>
             <AppRoutes />
-        </Router>
+        </>
     );
 }
 
